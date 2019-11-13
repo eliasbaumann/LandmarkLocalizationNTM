9956939fc0684c127a6f69b214d5457ec061a58d
Elias Baumann <baumaneq@hu-berlin.de>
2019-11-13 14:05:23 +0100

    added padding+expand_dims

--- heatmapgen.py
+++ heatmapgen.py
@@ -3,8 +3,9 @@ import tensorflow_probability as tfp
 import matplotlib.pyplot as plt
 
 class Heatmap_Generator(object):
-    def __init__(self, image_shape, sigma):
+    def __init__(self, image_shape, n_landmarks, sigma):
         self.image_shape = image_shape
+        self.n_landmarks = n_landmarks
         self.sigma = sigma
         
 
@@ -13,16 +14,11 @@ class Heatmap_Generator(object):
         idx = tf.concat([tf.reshape(X, [-1, 1]), tf.reshape(Y,[-1,1])], axis =1)
         gaussian = tfp.distributions.MultivariateNormalDiag(loc=coords, scale_diag=tf.ones(tf.shape(coords),tf.float32)*self.sigma)
         prob = tf.reshape(gaussian.prob(idx),tf.shape(X))
-        return prob
+        return tf.expand_dims(prob,-1)
     
     def generate_heatmaps(self, coord_list):
         hm_list = tf.map_fn(self._generate_heatmap, coord_list)
+        paddings = [[0,self.n_landmarks-tf.shape(hm_list)[0]],[0,0],[0,0],[0,0]]
+        hm_list = tf.pad(hm_list, paddings , 'CONSTANT', 0.)
         return hm_list#tf.stack(hm_list,axis=0)
 
-
-
-# if __name__ == "__main__":
-#     hmg = Heatmap_Generator([100,100],2.)
-#     hm = hmg.generate_heatmaps([[4,4],[40,60]])
-#     plt.matshow(hm[1])
-#     plt.show()
