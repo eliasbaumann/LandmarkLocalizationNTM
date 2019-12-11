58d35a9f8bc0ad2290f2cf11471817e83e34cbd3
Elias Baumann <baumaneq@hu-berlin.de>
2019-12-11 10:58:50 +0100

    added coord dist as metric

--- run.py
+++ run.py
@@ -31,7 +31,7 @@ parser.add_argument('--optimizer_epsilon', type=float, default=1e-10,
                       help='Epsilon used for RMSProp optimizer.')
 
 # Training options.
-parser.add_argument('--num_training_iterations', type=int, default=1000,
+parser.add_argument('--num_training_iterations', type=int, default=100,
                         help='Number of iterations to train for.')
 parser.add_argument('--report_interval', type=int, default=10,
                         help='Iterations between reports (samples, valid loss).')
@@ -72,13 +72,16 @@ class dnc_model(tf.keras.Model):
 
 # TODO: replace by pixelwise differnce
 def loss_func(gt_labels, logits):
-    # logit_keypoints = tf.map_fn(lambda x: tf.map_fn(get_max_indices, x), logits)
-    # gt_keypoints = tf.map_fn(lambda y: tf.map_fn(get_max_indices, y), gt_labels)
     loss = tf.nn.l2_loss(gt_labels-logits) / args.batch_size
     return loss
 
+def coord_dist(y_true, y_pred):
+    y_pred = tf.map_fn(lambda x: tf.map_fn(get_max_indices, x), y_pred)
+    y_true = tf.map_fn(lambda y: tf.map_fn(get_max_indices, y), y_true)
+    return tf.keras.losses.MeanAbsoluteError()(y_true,y_pred)#tf.nn.l2_loss(y_true-y_pred) /args.batch_size
+
+
 def get_max_indices(logits):
-    # coords = tf.cond(tf.equal(tf.reduce_max(logits), 0.),true_fn=lambda: tf.constant([0,0]),false_fn=lambda: tf.squeeze(tf.where(tf.equal(logits, tf.reduce_max(logits)))))
     coords = tf.squeeze(tf.where(tf.equal(logits, tf.reduce_max(logits))))
     coords = tf.cond(tf.greater(tf.rank(coords),tf.constant(1)),true_fn=lambda:tf.gather(coords,0),false_fn=lambda:coords)
     return tf.cast(coords, tf.float32)
@@ -86,19 +89,14 @@ def get_max_indices(logits):
 def train_unet(num_training_iterations, report_interval):
     dataset = data.Data_Loader(args.dataset, args.batch_size)
     dataset()
-    iterator = iter(dataset.data)
-    val_iterator = iter(dataset.val_data)
-    test_iterator = iter(dataset.test_data)
-
-    unet_model = unet.unet2d(128,2,[[2,2],[2,2],[2,2],[2,2]],dataset.n_landmarks)
-    #unet_model = unet.convnet2d(128, dataset.n_landmarks)
-    # unet_model.build(tf.TensorShape([args.batch_size, dataset.n_landmarks, 128, 125]))
+    # unet_model = unet.unet2d(128,2,[[2,2],[2,2],[2,2],[2,2]],dataset.n_landmarks)
+    unet_model = unet.convnet2d(128, dataset.n_landmarks)
 
     optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-07)
     
     n_epochs = 5 # TODO
 
-    unet_model.compile(optimizer, loss = loss_func)
+    unet_model.compile(optimizer, loss = tf.keras.losses.MeanSquaredError(), metrics= [coord_dist])
     unet_model.fit(x=dataset.data, epochs=n_epochs, validation_data = dataset.val_data, steps_per_epoch=num_training_iterations//n_epochs, validation_steps=1) #TODO ntraining epochs
     # for epoch in range(n_epochs):
     #     for iteration in range(num_training_iterations):
@@ -112,17 +110,21 @@ def train_unet(num_training_iterations, report_interval):
     #         if(iteration % report_interval == 0):
     #             img, label = next(val_iterator)
     #             logits = unet_model(img)
-    #             val_loss = loss_func(label, logits)
+    #             val_loss = coord_dist(label, logits)
     #             tf.print('train_loss:')
     #             tf.print(loss)
     #             tf.print('val_loss:')
     #             tf.print(val_loss)
     #             tf.print('---------')
+    iterator = iter(dataset.data)
     for _ in range(5):
         img, label = next(iterator)
         pred = unet_model.predict(img)
         pred_keypoints = tf.map_fn(lambda x: tf.map_fn(get_max_indices, x), pred)
-        vis_points(img.numpy().squeeze()[0], tf.squeeze(pred_keypoints).numpy()[0],5)
+        lab_kp = tf.map_fn(lambda x: tf.map_fn(get_max_indices, x), label)
+        vis_points(img.numpy().squeeze()[0], pred_keypoints.numpy()[0],5)
+        plt.show()
+        vis_points(img.numpy().squeeze()[0], lab_kp.numpy()[0], 5)
         plt.show()
 
 
