166e16fa3701ab6b49e57d36d3e777dd6ac372e9
baumaneq <elias.baumann@informatik.hu-berlin.de>
2020-02-05 12:27:20 +0100

    added NTM impl. (needs testing, needs encoder)

--- ntm.py
+++ ntm.py
@@ -69,26 +69,26 @@ class NTMCell(tf.keras.layers.AbstractRNNCell):
         self._ds = tf.keras.layers.MaxPooling2D(pool_size=(8,8))
 
         # ########### TODO: Matrix mode:
-        # TODO trying this using the already done implementation of convlstm2d        
-        self._controller = tf.keras.layers.ConvLSTM2DCell(filters=self.num_heads+2*self.write_head_num+1, kernel_size=3) # This needs to output CxC matrix K_t, and maybe a second matrix which is then used for other parameters?
-        # with multiple heads this needs to scale up?
-        self.num_params_per_head = 1+1+(self.shift_range*2+1)+1
+        # # TODO trying this using the already done implementation of convlstm2d        
+        # self._controller = tf.keras.layers.ConvLSTM2DCell(filters=self.num_heads+2*self.write_head_num+1, kernel_size=3) # This needs to output CxC matrix K_t, and maybe a second matrix which is then used for other parameters?
+        # # with multiple heads this needs to scale up?
+        # self.num_params_per_head = 1+1+(self.shift_range*2+1)+1
 
-        self._ctrl2p = tf.keras.layers.Dense(units=self.num_heads*self.num_params_per_head, activation=None) # Outputs: keystrength scalar beta_t, interpolation gate scalar g_t, shift weighting vector s_t (length n_memory rows), sharpening scalar gamma_t
+        # self._ctrl2p = tf.keras.layers.Dense(units=self.num_heads*self.num_params_per_head, activation=None) # Outputs: keystrength scalar beta_t, interpolation gate scalar g_t, shift weighting vector s_t (length n_memory rows), sharpening scalar gamma_t
 
-        self._ctrl2o = tf.keras.layers.Conv2D()#TODO how should the output look like?
+        # self._ctrl2o = tf.keras.layers.Conv2D()#TODO how should the output look like?
         
         # ########### TODO: Embedding mode:
 
-        # self._controller = tf.keras.layers.LSTMCell(filters=x, kernel_size=3)
-        # self._conv = tf.keras.layers.Conv2D(kernel_size=3) #TODO
+        self._controller = tf.keras.layers.LSTMCell()
+        self._conv = tf.keras.layers.Conv2D(kernel_size=8, strides=4) #TODO
 
-        # self._flat = tf.keras.layers.Flatten()
-        # self._rs = tf.keras.layers.Reshape(target_shape=TODO)
-        # self._us = tf.keras.layers.Conv2DTranspose(size=[8,8])
+        self._flat = tf.keras.layers.Flatten()
+        self._rs = tf.keras.layers.Reshape(target_shape=TODO)
+        self._us = tf.keras.layers.Conv2DTranspose(size=[8,8])
 
-        # self.num_params_per_head = self.memory_vector_dim + 1 + 1 + (self.shift_range * 2 + 1) + 1
-        # self.total_param_num = self.num_params_per_head * self.num_heads + self.memory_vector_dim * 2 * self.write_head_num
+        self.num_params_per_head = self.memory_vector_dim + 1 + 1 + (self.shift_range * 2 + 1) + 1
+        self.total_param_num = self.num_params_per_head * self.num_heads + self.memory_vector_dim * 2 * self.write_head_num
 
 
 
@@ -98,21 +98,125 @@ class NTMCell(tf.keras.layers.AbstractRNNCell):
         self.output_dim = output_dim
         self.shift_range = shift_range
 
+        # o2p = controller output to parameters, o2o = controller outputs to output
+
         self.o2p_initializer = create_linear_initializer(self.controller_units)
         self.o2o_initializer = create_linear_initializer(self.controller_units + self.memory_vector_dim * self.read_head_num)
+
+        self.o2p = tf.keras.layers.Dense(units=self.total_param_num, use_bias=True, kernel_initializer=self.o2p_initializer)
+        self.o2o = tf.keras.layers.Dense(units=self.output_dim, use_bias=True, kernel_initializer=self.o2o_initializer)
     
     def __call__(self, x, prev_state):
+        # TODO need to include the encoding/decoding -> we could also source this out, so that we don't have to change as much??, interesting...
         prev_read_list = prev_state.read_list
 
-        controller_input = tf.concat([x] + prev_read_list, axis=0) #TODO this will not work for me, need to change
+        controller_input = tf.concat([x] + prev_read_list, axis=0) #TODO this likely wont work, change when you fully understand whats happening
         #with tf.variable_scope('controller', reuse=self.reuse):
         controller_output, controller_state = self._controller(controller_input, prev_state.controller_state)
+        parameters = self.o2p(controller_output)
+        parameters = tf.clip_by_value(parameters, -self.clip_value, self.clip_value)
+
+        head_parameter_list = tf.split(parameters[:, :self.num_parameters_per_head * self.num_heads], self.num_heads, axis=1)
+        erase_add_list = tf.split(parameters[:, self.num_parameters_per_head * self.num_heads:], 2 * self.write_head_num, axis=1)
+
+        prev_w_list = prev_state.w_list
+        prev_M = prev_state.M
+        w_list = []
         
+        # create params
+        for i, head_parameter in enumerate(head_parameter_list):
+            k = tf.tanh(head_parameter[:, 0:self.memory_vector_dim])
+            beta = tf.nn.softplus(head_parameter[:, self.memory_vector_dim])
+            g = tf.sigmoid(head_parameter[:, self.memory_vector_dim + 1])
+            s = tf.nn.softmax(head_parameter[:, self.memory_vector_dim + 2:self.memory_vector_dim + 2 + (self.shift_range * 2 + 1)])
+            gamma = tf.nn.softplus(head_parameter[:, -1]) + 1
+            with tf.variable_scope('addressing_head_%d' % i):
+                w = self._addressing(k, beta, g, s, gamma, prev_M, prev_w_list[i])
+            w_list.append(w)
+        
+        # read
+        read_w_list = w_list[:self.read_head_num]
+        read_vector_list = [tf.reduce_sum(tf.expand_dims(read_w_list[i], dim=2)*prev_M, axis=1) for i in range(self.read_head_num)]
+        
+        # write
+        write_w_list = w_list[self.read_head_num:]
+        M = prev_M
+        for i in range(self.write_head_num):
+            w = tf.expand_dims(write_w_list[i], axis=2)
+            erase_vec = tf.expand_dims(tf.sigmoid(erase_add_list[i*2]), axis=1)
+            add_vec = tf.expand_dims(tf.tanh(erase_add_list[i*2+1]), axis=1)
+            M = M * (tf.ones(M.get_shape()) - tf.matmul(w, erase_vec)) + tf.matmul(w, add_vec)
+
+        ntm_output = self.o2o(tf.concat([controller_output] + read_vector_list, axis=1))
+        ntm_output = tf.clip_by_value(ntm_output, -self.clip_value, self.clip_value)
+        self.step += 1
+        return ntm_output, NTMControllerState(controller_state=controller_state, read_vector_list=read_vector_list, w_list=w_list, M=M)
+
+    def _addressing(self, k, beta, g, s, gamma, prev_M, prev_w):
+        # content focussing:
+        K = self._similarity(k, prev_M, method='cosine')
+        K_amplified = tf.exp(tf.expand_dims(beta, axis=1)*K)
+        w_c = K_amplified / tf.reduce_sum(K_amplified, axis=1, keepdims=True)
+
+        # location focussing:
+        g = tf.expand_dims(g, axis=1)
+        w_g = g * w_c + (1 - g) * prev_w
+
+        # TODO i literally do not know whats happening here, we create the shift matrix i guess
+        s = tf.concat([s[:, :self.shift_range + 1],
+                       tf.zeros([s.get_shape()[0], self.memory_size - (self.shift_range * 2 + 1)]),
+                       s[:, -self.shift_range:]], axis=1)
+        t = tf.concat([tf.reverse(s, axis=[1]), tf.reverse(s, axis=[1])], axis=1)
+        s_matrix = tf.stack(
+            [t[:, self.memory_size - i - 1:self.memory_size * 2 - i - 1] for i in range(self.memory_size)],
+            axis=1
+        )
+
+        w_ = tf.reduce_sum(tf.expand_dims(w_g, axis=1)*s_matrix, axis=2)
+        w_sharpened = tf.pow(w_, tf.expand_dims(gamma, axis=1))
+        w = w_sharpened / tf.reduce_sum(w_sharpened, axis=1, keepdims=True)
+        return w
+
+    # u = k, v = M
+    def _similarity(self,u,v, method='cosine'):
+        '''
+        Evaluates similarity between key vector and every row in Memory
+        Implemented: cosine similarity
+        '''
+        if method=='cosine':
+            nom = tf.matmul(v, u)
+            u_norm = tf.sqrt(tf.reduce_sum(tf.square(u), axis=1, keepdims=True))
+            v_norm = tf.sqrt(tf.reduce_sum(tf.square(v), axis=2, keepdims=True))
+            denom = u_norm * v_norm
+            return tf.squeeze(tf.math.divide_no_nan(nom,denom)) # instead of adding 1e-8 
+    
+    # TODO implement other init modes if necessary
+    def zero_state(self, batch_size, dtype):
+        read_vector_list = [self._expand(tf.tanh(self._learned_init(self.memory_vector_dim)), dim=0, N=batch_size) for i in range(self.read_head_num)]
+        w_list = [self._expand(tf.nn.softmax(self._learned_init(self.memory_size)), dim=0, N=batch_size) for i in range(self.read_head_num + self.write_head_num)]
+
+        controller_init_state = self.controller.zero_state(batch_size, dtype)
+
+        if self.init_mode == 'random':
+            M = self._expand(tf.tanh(tf.reshape(learned_init(self.memory_size * self.memory_vector_dim), [self.memory_size, self.memory_vector_dim])), dim=0, N=batch_size)
+        return NTMControllerState(controller_state=controller_init_state, read_vector_list=read_vector_list, w_list=w_list, M=M)
+
+
+    def _expand(self, x, dim, N):
+        return tf.concat([tf.expand_dims(x, dim) for _ in range(N)], axis=dim)
+
+    def _learned_init(self, units):
+        return tf.squeeze(tf.keras.layers.Dense(units, activation_fn=None, biases_initializer=None)(tf.ones([1, 1])))
 
     @property
     def state_size(self):
-        return NTMControllerState()
+        return NTMControllerState(
+            controller_state=self.controller.state_size,
+            read_vector_list=[self.memory_vector_dim for _ in range(self.read_head_num)],
+            w_list=[self.memory_size for _ in range(self.read_head_num + self.write_head_num)],
+            M=tf.TensorShape([self.memory_size * self.memory_vector_dim]))
 
     @property
     def output_size(self):
         return self.output_dim
+
