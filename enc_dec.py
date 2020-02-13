9d8f5a06c906b2296183ed22d371698b33247eaa
Elias Baumann <baumaneq@hu-berlin.de>
2020-02-13 16:00:10 +0100

    fxied enc-dec architecture

--- enc_dec.py
+++ enc_dec.py
@@ -10,25 +10,27 @@ class Encoder_Decoder_Wrapper(tf.keras.layers.Layer):
         super(Encoder_Decoder_Wrapper, self).__init__()
         self.num_filters = num_filters
         self.kernel_size = kernel_size
+        self.pool_size = pool_size
         self.batch_size = batch_size
-        self.conv = [tf.keras.layers.Conv2D(filters=num_filters, kernel_size=self.kernel_size, activation='relu', padding='same', data_format='channels_first') for _ in range(4)]
+        self.conv = [tf.keras.layers.Conv2D(filters=num_filters, kernel_size=self.kernel_size, activation='relu', padding='same', data_format='channels_first') for _ in range(5)]
 
         self.conv_enc = tf.keras.layers.Conv2D(filters=1, kernel_size=self.kernel_size, activation='relu', padding='same', data_format='channels_first')
-        self.conv_out = tf.keras.layers.Conv2D(filters=1, kernel_size=self.kernel_size, activation='relu', padding='same', data_format='channels_first')
-        self.ds = tf.keras.layers.AveragePooling2D(pool_size=pool_size, strides=pool_size, data_format='channels_first')
-        self.us = tf.keras.layers.UpSampling2D(size=pool_size, data_format='channels_first')
+        self.ds = tf.keras.layers.AveragePooling2D(pool_size=self.pool_size, strides=self.pool_size, data_format='channels_first')
+        self.us = tf.keras.layers.UpSampling2D(size=self.pool_size, data_format='channels_first')
+
+        self.flat = tf.keras.layers.Flatten(data_format='channels_first')
 
         self.cell = NTMCell(controller_units=256, memory_size=64, memory_vector_dim=256, read_head_num=3, write_head_num=3, output_dim=256) # TODO
 
 
     def call(self, inputs):
+        
         x = self.conv[0](inputs) 
         x = self.ds(x)
         x = self.conv[1](x)
         x = self.ds(x)
         x = self.conv_enc(x)
-        x = tf.reshape(x, [self.batch_size, -1])
-        x.set_shape([self.batch_size, 256])
+        x = self.flat(x)
 
         # NTM here
         state = self.cell.get_initial_state(batch_size=self.batch_size, dtype=tf.float32)
@@ -39,5 +41,5 @@ class Encoder_Decoder_Wrapper(tf.keras.layers.Layer):
         x = self.us(x)
         x = self.conv[3](x)
         x = self.us(x)
-        x = self.conv_out(x)
+        x = self.conv[4](x)
         return x
