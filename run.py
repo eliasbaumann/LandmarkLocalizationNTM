7552af589c613c4412e62191a97a3139c0b6a151
Elias Baumann <baumaneq@hu-berlin.de>
2020-02-17 14:05:00 +0100

    stores sample preds now

--- run.py
+++ run.py
@@ -31,15 +31,15 @@ parser.add_argument('--optimizer_epsilon', type=float, default=1e-10,
                       help='Epsilon used for RMSProp optimizer.')
 
 # Training options.
-parser.add_argument('--num_training_iterations', type=int, default=10,
+parser.add_argument('--num_training_iterations', type=int, default=10000,
                         help='Number of iterations to train for.')
-parser.add_argument('--num_epochs', type=int, default=2,
+parser.add_argument('--num_epochs', type=int, default=200,
                         help='Number of iterations to train for.')
 parser.add_argument('--report_interval', type=int, default=10,
                         help='Iterations between reports (samples, valid loss).')
 parser.add_argument('--checkpoint_dir', type=str, default='/tmp/tf/dnc',
                        help='Checkpointing directory.')
-parser.add_argument('--checkpoint_interval', type=int, default=5,
+parser.add_argument('--checkpoint_interval', type=int, default=20,
                         help='Checkpointing step interval.')
 
 args = parser.parse_args()
@@ -54,26 +54,6 @@ def vis_points(image, points, diameter=5, given_kp=None):
         for (y,x) in given_kp:
             cv2.circle(im, (int(x), int(y)), diameter, (0, 255, 0), -1)
     plt.imshow(im)
-
-# class dnc_model(tf.keras.Model):
-#     def __init__(self, output_size):
-#         super(dnc_model, self).__init__()
-#         self.output_size = output_size
-    
-#     def call(self, inputs):
-#         access_config = {
-#             "memory_size": args.memory_size,
-#             "word_size": args.word_size,
-#             "num_reads": args.num_read_heads,
-#             "num_writes": args.num_write_heads}
-#         controller_config = {
-#             "hidden_layers": args.hidden_layers,
-#             "hidden_size": args.hidden_size}
-#         clip_value = args.clip_value
-#         cell = dnccell.DNCCell(access_config, controller_config, self.output_size, clip_value)
-#         initial_state = cell.initial_state(args.batch_size)
-#         output_sequence, _ = tf.keras.layers.RNN(cell, time_major=True)(inputs, initial_state=initial_state)
-#         return output_sequence
 
 def loss_func(gt_labels, logits):
     loss = tf.nn.l2_loss(gt_labels-logits) / args.batch_size
@@ -91,50 +71,53 @@ def get_max_indices(logits):
     return tf.cast(coords, tf.float32)
 
 def unstack_img_lab(img, kp_list):
+    given_kp = tf.gather(img, tf.constant([0]), axis=0)    
     given_kp = tf.gather(img, tf.constant(range(1,len(kp_list))), axis=0)
     given_kp = tf.map_fn(get_max_indices, given_kp)
     image = tf.gather(img, tf.constant(0), axis=0)
     return image, given_kp
 
-def vis_results(img,label,model,kp_list):
+def store_results(img, label, model, kp_list, i, path):
     given_kp = None
-    # if kp_list is not None:
-    #     given_kp = tf.gather(img, tf.constant(range(1,len(kp_list))), axis=0)
-    #     given_kp = tf.map_fn(get_max_indices, given_kp)
-    #     img = tf.gather(img, tf.constant(0), axis=0)
     img = tf.expand_dims(img, 0)
-
     label = tf.expand_dims(label, 0)
     pred = model.predict(img)
     if kp_list is not None:
         img, given_kp = unstack_img_lab(tf.squeeze(img), kp_list)
-
     pred_keypoints = tf.map_fn(lambda x: tf.map_fn(get_max_indices, x), pred)
     lab_kp = tf.map_fn(lambda x: tf.map_fn(get_max_indices, x), label)
     vis_points(img.numpy().squeeze(), pred_keypoints.numpy()[0], 5, given_kp)
-    plt.show()
+    if not os.path.exists(path+'\\samples\\'):
+        os.makedirs(path+'\\samples\\')
+    plt.savefig(path+'\\samples\\%02d_pred.png' % i)
     vis_points(img.numpy().squeeze(), lab_kp.numpy()[0], 5, given_kp)
-    plt.show()
+    plt.savefig(path+'\\samples\\%02d_gt.png' % i)
 
-def predict_from_cp(path, run_number, kp_list=None, ntm=False):
-    cp_dir = load_dir(path, run_number)
+def predict_from_cp(path, run_number, num_filters, fmap_inc_factor, ds_factors, kp_list=None, ntm=False):
+    if kp_list == [0]:
+        kp_list = None
+    log_dir, cp_dir = load_dir(path, run_number)
     dataset = data.Data_Loader(args.dataset, args.batch_size)
     dataset(keypoints=kp_list)
     latest = tf.train.latest_checkpoint(cp_dir)
-    unet_model = unet.unet2d(128,2,[[2,2],[2,2],[2,2],[2,2]],dataset.n_landmarks-(len(kp_list)-1), ntm=ntm, batch_size=args.batch_size)
+    len_kp = (len(kp_list)-1) if kp_list is not None else 0
+    unet_model = unet.unet2d(num_filters, fmap_inc_factor, ds_factors, dataset.n_landmarks-len_kp, ntm=ntm, batch_size=args.batch_size)
     optimizer = tf.keras.optimizers.Adam(learning_rate=args.learning_rate, beta_1=0.9, beta_2=0.999, epsilon=1e-07)
     unet_model.compile(optimizer, loss = loss_func, metrics= [coord_dist])
     unet_model.load_weights(latest)
     iterator = iter(dataset.test_data)
-    for _ in range(5):
+    for i in range(5):
         img, label = next(iterator)
-        vis_results(img, label, unet_model,kp_list)
+        store_results(img, label, unet_model, kp_list, i, log_dir)
 
-def train_unet(path, kp_list=None, ntm=False):
+def train_unet(path, num_filters, fmap_inc_factor, ds_factors, kp_list=None, ntm=False):
+    if kp_list == [0]:
+        kp_list = None
     log_path, cp_path = create_dir(path)
     dataset = data.Data_Loader(args.dataset, args.batch_size)
     dataset(keypoints=kp_list)
-    unet_model = unet.unet2d(64,2,[[2,2],[2,2],[2,2],[2,2]],dataset.n_landmarks-(len(kp_list)-1), ntm=ntm, batch_size=args.batch_size)
+    len_kp = (len(kp_list)-1) if kp_list is not None else 0
+    unet_model = unet.unet2d(num_filters, fmap_inc_factor, ds_factors, dataset.n_landmarks-len_kp, ntm=ntm, batch_size=args.batch_size)
     #unet_model = unet.convnet2d(128, dataset.n_landmarks)
     optimizer = tf.keras.optimizers.Adam(learning_rate=args.learning_rate, beta_1=0.9, beta_2=0.999, epsilon=1e-07)    
     tb_callback = tf.keras.callbacks.TensorBoard(log_dir=log_path)
@@ -149,9 +132,9 @@ def train_unet(path, kp_list=None, ntm=False):
                    callbacks=[tb_callback, cp_callback]) 
     unet_model.summary()
     iterator = iter(dataset.test_data)
-    for _ in range(5): # TODO the problem might be that i can only predict a batch, not a single file anymore? because of previous_read_list?
+    for i in range(5): # TODO the problem might be that i can only predict a batch, not a single file anymore? because of previous_read_list?
         img, label = next(iterator)
-        vis_results(img, label, unet_model,kp_list)
+        store_results(img, label, unet_model,kp_list, i, log_path)
 
 def create_dir(path):
     previous_runs = os.listdir(path)
@@ -170,10 +153,13 @@ def load_dir(path, run_number):
     l_dir = os.path.join(path, logdir)
     cp_pth = l_dir+'\\cp\\cp-{epoch:04d}.ckpt'
     cp_dir = os.path.dirname(cp_pth)
-    return cp_dir
+    return l_dir, cp_dir
 
 
 if __name__ == "__main__":
     PATH = 'C:\\Users\\Elias\\Desktop\\MA_logs'
-    train_unet(PATH, kp_list = [0,1,2], ntm=True)
-    # predict_from_cp(PATH, 1, kp_list = [0,1,2], ntm=True)
+    train_unet(PATH, num_filters=64, fmap_inc_factor=2, ds_factors=[[2,2],[2,2],[2,2],[2,2],[2,2]], kp_list=None, ntm=False)
+    # predict_from_cp(PATH, 3, num_filters=64, fmap_inc_factor=2, ds_factors=[[2,2],[2,2],[2,2],[2,2]], kp_list = None, ntm=False)
+
+
+# kp_list: 0 is image, remaining numpers are keypoints. If you dont want to include keypoints in input, set to None
