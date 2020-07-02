10aaf28a64336d484a7daf73498e5c0dd1bc9daf
Elias Baumann <baumaneq@hu-berlin.de>
2020-07-02 14:08:12 +0200

    added cephal, fixed shuffle

--- data.py
+++ data.py
@@ -9,18 +9,18 @@ import numpy as np
 from heatmapgen import generate_heatmaps
 import matplotlib.pyplot as plt
 
-PATH = 'C:/Users/Elias/Desktop/Landmark_Datasets/'
-
 @tf.function
 def decode_image(file_path):
     img = tf.io.read_file(file_path)
-    img = tf.cast(tf.io.decode_jpeg(img), tf.float32)
-    img = tf.image.per_image_standardization(img)
+    img = tf.cast(tf.io.decode_jpeg(img, channels=1), tf.float32)
+    img = tf.scalar_mul(tf.constant(1./255., dtype=tf.float32), img)
+    # img = tf.image.per_image_standardization(img)
     # img = tf.subtract(tf.scalar_mul(tf.constant(2., dtype=tf.float32), img),tf.constant(1., dtype=tf.float32))
     return img
 
 class Data_Loader():
-    def __init__(self, name, batch_size, train_pct=80, val_pct=10, test_pct=10, repeat=True, prefetch=True, n_aug_rounds=5, sigma=1.):
+    def __init__(self, data_path, name, batch_size, train_pct=80, val_pct=10, test_pct=10, repeat=True, prefetch=True, n_aug_rounds=5, sigma=1.):
+        self.path = data_path
         self.name = name
         self.batch_size = batch_size
         self.train_pct = train_pct
@@ -43,14 +43,16 @@ class Data_Loader():
     def __call__(self, im_size=None, keypoints=None):
         print("Creating Datasets...")
         self.keypoints = keypoints
-
-        data = self.load_droso()
-            
-        if self.name == 'cephal':
+        if self.name == 'droso':
+            data = self.load_droso()
+        elif self.name == 'cephal':
             data = self.load_cephal()
+        else:
+            print("No correct dataset name given, please select from droso and cephal, defaulting to droso")
+            data = self.load_droso()
         imx, imy = im_size
         data = self.resize_images(data, imx, self.orig_im_size[0], self.orig_im_size[1])
-        # data = data.shuffle(buffer_size=self.ds_size) #TODO add this for actual runs, 
+        #data = data.shuffle(buffer_size=self.ds_size, reshuffle_each_iteration=False) #TODO add this for actual runs, 
         
         # train test val split (take and skip)
         n_train_obs = int(self.ds_size * (self.train_pct/100.0) - (self.ds_size * (self.train_pct/100.0) % self.batch_size))
@@ -66,6 +68,10 @@ class Data_Loader():
         if self.keypoints is not None:
             self.train_data, self.val_data, self.test_data = self.kp_to_input(self.train_data, self.val_data, self.test_data, self.keypoints)
         
+        self.train_data = self.train_data.shuffle(buffer_size=n_train_obs, reshuffle_each_iteration=False)
+        self.val_data = self.train_data.shuffle(buffer_size=n_val_obs, reshuffle_each_iteration=False)
+        self.test_data = self.train_data.shuffle(buffer_size=n_test_obs, reshuffle_each_iteration=False)
+
         if self.repeat:
             self.train_data = self.train_data.repeat()
             self.val_data = self.val_data.repeat()
@@ -96,7 +102,7 @@ class Data_Loader():
             resized = tf.image.resize(img, [imx,imx]) #TODO correct axis
             resized = tf.transpose(resized, perm=[2,0,1]) # convert to channels first
             resized.set_shape([1,imx,imx])
-            keypoints.set_shape([40,imx,imx])
+            keypoints.set_shape([self.n_landmarks,imx,imx])
             return resized, keypoints, fn
 
         def convert_all(images, keypoints, filename):
@@ -108,19 +114,34 @@ class Data_Loader():
             image = np.concatenate((image,keypoints),axis=0)
             image = np.transpose(image, (1,2,0))
             sometimes = lambda aug: iaa.Sometimes(0.5, aug)
-            seq = iaa.Sequential([
-                                  iaa.Fliplr(.3),
-                                  iaa.Flipud(.3),
-                                  sometimes(iaa.CropAndPad(percent=(-0.25, 0.25), pad_mode="constant", pad_cval=1e-5)),
-                                  sometimes(iaa.Affine(scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
-                                                       translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
-                                                       rotate=(-45, 45),
-                                                       shear=(-16, 16),
-                                                       order=[0, 1],
-                                                       cval=1e-5,
-                                                       mode="constant")),
-                                  sometimes(iaa.ElasticTransformation(alpha=(0.0, 40.0), sigma=(4.0, 8.0), cval=1e-5, mode="constant")),
-                                  ], random_order=True)
+            if self.name == 'droso':
+                seq = iaa.Sequential([
+                                    iaa.Fliplr(.3),
+                                    iaa.Flipud(.3),
+                                    sometimes(iaa.CropAndPad(percent=(-0.25, 0.25), pad_mode="constant", pad_cval=1e-5)),
+                                    sometimes(iaa.Affine(scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+                                                        translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
+                                                        rotate=(-45, 45),
+                                                        shear=(-16, 16),
+                                                        order=[0, 1],
+                                                        cval=1e-5,
+                                                        mode="constant")),
+                                    sometimes(iaa.ElasticTransformation(alpha=(0.0, 40.0), sigma=(4.0, 8.0), cval=1e-5, mode="constant")),
+                                    ], random_order=True)
+            elif self.name == 'cephal': # no up down flipping, reduced rotate, could reduce this even more, because images don't really differ here..
+                seq = iaa.Sequential([
+                                    sometimes(iaa.CropAndPad(percent=(-0.25, 0.25), pad_mode="constant", pad_cval=1e-5)),
+                                    sometimes(iaa.Affine(scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+                                                        translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
+                                                        rotate=(-15, 15),
+                                                        shear=(-16, 16),
+                                                        order=[0, 1],
+                                                        cval=1e-5,
+                                                        mode="constant")),
+                                    sometimes(iaa.ElasticTransformation(alpha=(0.0, 40.0), sigma=(4.0, 8.0), cval=1e-5, mode="constant")),
+                                    ], random_order=True)
+            else:
+                seq  =  iaa.Sequential([iaa.ElasticTransformation(alpha=(0.0, 40.0), sigma=(4.0, 8.0), cval=1e-5, mode="constant")], random_order=True)
             with warnings.catch_warnings():
                 warnings.filterwarnings("ignore", category=UserWarning) # only doing this because imgaug warns me that my "image" is stacked with its heatmaps
                 image_aug = seq.augment_image(image=image)
@@ -136,7 +157,7 @@ class Data_Loader():
         def _augment(img, lab, fn):
             image, keypoints = tf.numpy_function(_albu_transform, [img, lab], [tf.float32, tf.float32])
             image.set_shape([1,imx,imy])
-            keypoints.set_shape([40,imx,imy])
+            keypoints.set_shape([self.n_landmarks,imx,imy])
             return image, keypoints, fn
 
 
@@ -173,18 +194,23 @@ class Data_Loader():
         return data.flat_map(resplit), val_data.flat_map(resplit), test_data.flat_map(resplit)
 
     def load_cephal(self):
-        self.n_landmarks = 19
-        data_dir = pathlib.Path(PATH+self.name+'/RawImage/')
-        list_im = tf.data.Dataset.list_files(str(data_dir)+'*/*')
+        self.n_landmarks = 20 # Actually 19, workaround so we can easily do the iterative stuff...
+        self.ds_size = 400
+        self.orig_im_size = tf.constant([1935,2400])
+        data_dir = pathlib.Path(self.path+self.name+'/images/')
+        list_im = tf.data.Dataset.list_files(str(data_dir)+'*/*') # has default shuffle
         
         @tf.function
         def process_path(file_path):
             img = decode_image(file_path)
+            img.set_shape([2400,1935,1]) # H,W,C
             file_name = tf.strings.split(tf.strings.split(file_path, sep='\\')[-1], sep='.')[0]
-            label = tf.strings.split(tf.io.read_file(PATH+self.name+'/400_senior/'+file_name+'.txt'),sep='\r\n')[:self.n_landmarks]
+            label = tf.strings.split(tf.io.read_file(self.path+self.name+'/raw/'+file_name+'.txt'),sep='\r\n')[:19]
             label = tf.map_fn(lambda x: tf.strings.split(x,sep=','),label)
             label = tf.strings.to_number(label, out_type=tf.dtypes.int32)
-            return img, label
+            w,h = tf.split(label, 2, axis=1)
+            label = tf.concat([h,w], axis=1)
+            return img, label, file_name
 
         return list_im.map(process_path, num_parallel_calls=tf.data.experimental.AUTOTUNE)
         
@@ -193,7 +219,7 @@ class Data_Loader():
         self.n_landmarks = 40
         self.ds_size = 471 # TODO change when full data available
         self.orig_im_size = tf.constant([3840,3234])
-        data_dir = pathlib.Path(PATH+self.name+'/images/')
+        data_dir = pathlib.Path(self.path+self.name+'/images/')
         list_im = tf.data.Dataset.list_files(str(data_dir)+'*.jpg')
 
         @tf.function
@@ -201,7 +227,7 @@ class Data_Loader():
             img = decode_image(file_path)
             img.set_shape([3234,3840,1]) # H, W, C
             file_name = tf.strings.split(tf.strings.split(file_path, sep='\\')[-1], sep='.')[0]
-            label = tf.strings.split(tf.io.read_file(PATH+self.name+'/raw/'+file_name+'.txt'), sep='\n')[:-1]
+            label = tf.strings.split(tf.io.read_file(self.path+self.name+'/raw/'+file_name+'.txt'), sep='\n')[:-1]
             label = tf.map_fn(lambda x: tf.strings.split(x, sep=' '), label)
             label = tf.strings.to_number(label, out_type=tf.dtypes.float32)
             w,h = tf.split(label,2, axis=1)
@@ -211,5 +237,3 @@ class Data_Loader():
             return img, label, file_name
         
         return list_im.map(process_path, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-        
-        #self.im_size = decode_image(str(PATH+self.name+'/images/NG-SP196-909-0001.jpg')).shape
