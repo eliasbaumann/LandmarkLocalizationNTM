86837e06b19b3e900b25b00863408bdeb46dc253
Elias Baumann <baumaneq@hu-berlin.de>
2020-10-27 17:12:24 +0100

    sigmoid

--- enc_dec.py
+++ enc_dec.py
@@ -31,7 +31,7 @@ class Encoder_Decoder_Wrapper(tf.keras.layers.AbstractRNNCell):
             self.read_head_num = ntm_config["ntm_param"]["read_head_num"]
             self.write_head_num = ntm_config["ntm_param"]["write_head_num"]
             self.init_mode = ntm_config["ntm_param"]["init_mode"]
-            
+                
             self.cell = NTMCell(controller_units=self.controller_units, memory_size=self.memory_size, memory_vector_dim=self.memory_vector_dim, read_head_num=self.read_head_num, write_head_num=self.write_head_num, output_dim=self.output_dim, batch_size=batch_size, init_mode=self.init_mode, name=self.name+"_cell") 
         else:
             self.controller_units = None
@@ -46,17 +46,18 @@ class Encoder_Decoder_Wrapper(tf.keras.layers.AbstractRNNCell):
         self.num_filters = ntm_config["enc_dec_param"]["num_filters"]
         self.kernel_size = ntm_config["enc_dec_param"]["kernel_size"]
         self.pool_size = ntm_config["enc_dec_param"]["pool_size"]
-        
+        self.reg = ActivityRegularizationLayer(1., self.name+'activity_regularization') if ntm_config["enc_dec_param"]["reg"] else ActivityRegularizationLayer(0., self.name+'activity_regularization') 
+
         self.dim = tf.sqrt(tf.cast(self.output_dim, tf.float32))
         self.conv = [tf.keras.layers.Conv2D(filters=self.num_filters, kernel_size=self.kernel_size, activation=tf.nn.leaky_relu, padding='same', data_format='channels_first', name=self.name+'enc_dec_conv_%d' % i) for i in range(len(self.pool_size)*2)]
-        self.conv_out = tf.keras.layers.Conv2D(filters=1, kernel_size=self.kernel_size, activation=tf.nn.leaky_relu, padding='same', data_format='channels_first', name=self.name+'enc_dec_out')
+        self.conv_out = tf.keras.layers.Conv2D(filters=1, kernel_size=self.kernel_size, activation=tf.keras.activations.sigmoid, padding='same', data_format='channels_first', name=self.name+'enc_dec_out')
         self.conv_enc = tf.keras.layers.Conv2D(filters=1, kernel_size=self.kernel_size, activation=tf.nn.leaky_relu, padding='same', data_format='channels_first', name=self.name+'enc_dec_last_enc')
         self.ds = [tf.keras.layers.AveragePooling2D(pool_size=i, strides=[i,i], data_format='channels_first', name=self.name+'enc_dec_ds_%d' % i) for i in self.pool_size]
         self.us = [tf.keras.layers.UpSampling2D(size=i, data_format='channels_first', name=self.name+'enc_dec_us_%d' % i) for i in self.pool_size[::-1]]
 
         self.flat = tf.keras.layers.Flatten(data_format='channels_first', name=self.name+'enc_dec_flat')
 
-        self.reg = ActivityRegularizationLayer(1., self.name+'activity_regularization')        
+        
 
     def call(self, x, state):
         for i in range(len(self.pool_size)):
